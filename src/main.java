2f3154398247d6fadadb968c77a941f0c4ce8d26
SFMZ <smartinezz1@miumg.edu.gt>
2021-09-10 23:24:08 -0600

    Segunda version saludo

--- src/main.java
+++ src/main.java
@@ -5,6 +5,14 @@ public class main {
     public static void main (String args[])
     {
         System.out.println("Hola mundo!");
+        saludo("Sulma");
+
+
     }
+
+    public static void saludo(String nombre) {
+        System.out.println("Hola "+ nombre + " !!!");
+    }
+
 }
 
