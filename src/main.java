4da14dad900a92adabdd3ac84cb3554d562468e9
SFMZ <smartinezz1@miumg.edu.gt>
2021-09-10 23:28:48 -0600

    final

--- src/main.java
+++ src/main.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
 
-        String nombre = scan.nextLine();
-
-        System.out.println(nombre);
-        StringBuilder strb = new StringBuilder(nombre);
-        nombre = strb.reverse().toString();
-        System.out.println(nombre);
+        System.out.println("ULTIMA VERSION!!!");
 
 
     }
