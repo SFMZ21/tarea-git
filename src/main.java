9b2d400dc8d2b9a470b4908fafc883d6af0ec8e6
SFMZ <smartinezz1@miumg.edu.gt>
2021-09-10 23:28:23 -0600

    imprimir una cadena al reves

--- src/main.java
+++ src/main.java
@@ -4,14 +4,13 @@ public class main {
 
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        int edad = scan.nextInt();
 
-        for (int i = 0; i <= edad; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        String nombre = scan.nextLine();
+
+        System.out.println(nombre);
+        StringBuilder strb = new StringBuilder(nombre);
+        nombre = strb.reverse().toString();
+        System.out.println(nombre);
 
 
     }
